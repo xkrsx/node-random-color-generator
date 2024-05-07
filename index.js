025517602def17ce630357b24c278990e89f808a
Jakub Markiewicz <markiewicz267@gmail.com>
2024-05-08 01:22:35 +0200

    Update index.js Printing random color hashtag box with hex color value

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 import { argv } from 'node:process';
 
 import chalk from 'chalk';
+import colors from 'color-name';
+import colorString from 'color-string';
 import randomColor from 'randomcolor';
 
-// taking input from user
 const hueFromUser = argv[2];
 const luminosityFromUser = argv[3];
 
-const ask = `Please type color name and luminosity after 'node index.js'`
+const askStyle = chalk.italic.yellow;
+const askText = `Please type color name and luminosity after 'node index.js'!`;
 
 let fontColor;
-if (!hueFromUser && !luminosityFromUser) {
-  fontColor = randomColor();
-} if (hueFromUser || luminosityFromUser === 'ask') {
-console.log(ask)
-}
+const boxWidth = 31;
+const boxSides = 5;
+const boxFrame = 3;
+const boxSpace = boxWidth - 2 * boxSides;
+fontColor = randomColor();
 
-console.log(chalk.hex(fontColor)('test'));
+const colorfulHashtag = chalk.hex(fontColor)('#');
+const boxSingleSpace = ' ';
+const boxTopBottom = `${colorfulHashtag.repeat(boxWidth)}\n`.repeat(boxFrame);
+
+const boxSide = `${(colorfulHashtag.repeat(boxSides)
+  + boxSingleSpace.repeat(boxSpace)
+  + colorfulHashtag.repeat(boxSides))}\n`;
+
+const boxText = `${(colorfulHashtag.repeat(boxSides)
+  + boxSingleSpace.repeat((boxSpace - fontColor.length) / 2)
+  + fontColor
+  + boxSingleSpace.repeat((boxSpace - fontColor.length) / 2)
+  + colorfulHashtag.repeat(boxSides))}\n`;
+
+const box = boxTopBottom + boxSide + boxText + boxSide + boxTopBottom;
+
+if (!hueFromUser && !luminosityFromUser) {
+  console.log(chalk.hex(fontColor)(box));
+}
+ else if (hueFromUser || luminosityFromUser === 'ask') {
+console.log(askStyle(askText));
+}
+//  else if {hueFromUser
+
+  // console.log(chalk.hex(colors.hueFromUser)('test'));
+
+//  }
+
+const test = `${colors}.${hueFromUser}`;
+console.log(colorString.to.hex(test));

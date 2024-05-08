2ea0a25cb6a73a17692fa4f60118ee35a39e4978
Jakub Markiewicz <markiewicz267@gmail.com>
2024-05-08 02:00:14 +0200

    Update index.js Working on accepting user's input

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import { argv } from 'node:process';
 
 import chalk from 'chalk';
-import colors from 'color-name';
-import colorString from 'color-string';
 import randomColor from 'randomcolor';
 
 const hueFromUser = argv[2];
@@ -11,12 +9,12 @@ const luminosityFromUser = argv[3];
 const askStyle = chalk.italic.yellow;
 const askText = `Please type color name and luminosity after 'node index.js'!`;
 
-let fontColor;
 const boxWidth = 31;
 const boxSides = 5;
 const boxFrame = 3;
 const boxSpace = boxWidth - 2 * boxSides;
-fontColor = randomColor();
+
+let fontColor = randomColor();
 
 const colorfulHashtag = chalk.hex(fontColor)('#');
 const boxSingleSpace = ' ';
@@ -28,23 +26,25 @@ const boxSide = `${(colorfulHashtag.repeat(boxSides)
 
 const boxText = `${(colorfulHashtag.repeat(boxSides)
   + boxSingleSpace.repeat((boxSpace - fontColor.length) / 2)
-  + fontColor
+  + chalk.hex(fontColor)(fontColor)
   + boxSingleSpace.repeat((boxSpace - fontColor.length) / 2)
   + colorfulHashtag.repeat(boxSides))}\n`;
 
 const box = boxTopBottom + boxSide + boxText + boxSide + boxTopBottom;
 
-if (!hueFromUser && !luminosityFromUser) {
-  console.log(chalk.hex(fontColor)(box));
+// if (!hueFromUser && !luminosityFromUser) {
+//   console.log(chalk.hex(fontColor)(box));
+// }
+//  else if (hueFromUser || luminosityFromUser === 'ask') {
+// console.log(askStyle(askText));
+// }
+
+if (hueFromUser) {
+
+  const createUserColorBox = (userInput) => {
+    fontColor = randomColor({hue: userInput})
+    
+   console.log(chalk.hex('red')(box))
 }
- else if (hueFromUser || luminosityFromUser === 'ask') {
-console.log(askStyle(askText));
+createUserColorBox(hueFromUser)
 }
-//  else if {hueFromUser
-
-  // console.log(chalk.hex(colors.hueFromUser)('test'));
-
-//  }
-
-const test = `${colors}.${hueFromUser}`;
-console.log(colorString.to.hex(test));
